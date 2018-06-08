98b1007b07fd8f4bca052142398d43077268ae97
thavoo <gustavoh.2312@gmail.com>
2018-06-08 04:24:48 -0400

    errores vista

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>403</title>
+        <title>404</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -70,7 +70,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    403 - Accion no autorizada
+                    404 - No existe esta ruta
                 </div>
 
                 <div class="links">
